28491b57a7fd39bc470bc54b66aeefb651b74c77
Menelaos Zetas <menelaoszetas@gmail.com>
2017-07-12 19:16:34 +0300

    setting onClickListener to the donate button

--- app/src/main/java/com/example/android/musicapp/Payment.java
+++ app/src/main/java/com/example/android/musicapp/Payment.java
@@ -1,3 +1,5 @@
+// Based to https://www.youtube.com/watch?v=mTcM8sYZHas
+
 package com.example.android.musicapp;
 
 import android.app.Activity;
@@ -5,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.paypal.android.sdk.payments.PayPalConfiguration;
@@ -32,35 +35,42 @@ public class Payment extends AppCompatActivity {
         m_response = (TextView) findViewById(R.id.response);
 
         m_configuration = new PayPalConfiguration()
-            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX) //for the test. needs production for real
-            .clientId(m_paypalClientId);
+                .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX) //for the test. needs production for real
+                .clientId(m_paypalClientId);
 
         m_service = new Intent(this, PayPalService.class);
         m_service.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
         startService(m_service); // paypal service, listening to calls to paypal app
+
+        Button donate = (Button) findViewById(R.id.button1P);
+
+        donate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                PayPalPayment payment = new PayPalPayment(new BigDecimal(10), "USD", "Test payment", PayPalPayment.PAYMENT_INTENT_SALE);
+
+                Intent intent = new Intent(Payment.this, PaymentActivity.class);
+                intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
+                intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+                startActivityForResult(intent, m_paypalRequestCode);
+            }
+        });
+
     }
 
-    public void donate(View view){
-        PayPalPayment payment = new PayPalPayment(new BigDecimal(10), "USD", "Test payment", PayPalPayment.PAYMENT_INTENT_SALE);
-
-        Intent intent = new Intent(this, PaymentActivity.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
-        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
-        startActivityForResult(intent, m_paypalRequestCode);
-    }
-
-    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == m_paypalRequestCode){
-            if(resultCode == Activity.RESULT_OK){
+        if (requestCode == m_paypalRequestCode) {
+            if (resultCode == Activity.RESULT_OK) {
                 //payment confirmation
                 PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
 
-                if(confirmation != null){
+                if (confirmation != null) {
                     String state = confirmation.getProofOfPayment().getState();
 
-                    if(state.equals("approved")){
+                    if (state.equals("approved")) {
                         m_response.setText("payment approved");
                     } else {
                         m_response.setText("error in the payment");
